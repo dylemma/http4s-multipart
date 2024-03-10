cf37daab50592c81784e76d610e58052e7b92b42
Dylan Halperin <dylan@dylemma.io>
2024-03-09 19:43:29 -0500

    add some comments to Main

--- src/main/scala/io/dylemma/sandbox/Main.scala
+++ src/main/scala/io/dylemma/sandbox/Main.scala
@@ -20,6 +20,8 @@ object Main extends IOApp {
 
 		def utf8Chunk(text: String) = PartChunk(Chunk.array(text.getBytes(StandardCharsets.UTF_8)))
 
+		// Pretend we have access to `org.http4s.multipart.MultipartParser.parseEvents`
+		// and we've fed a `multipart/form-data` entity through that pipe.
 		val exampleEvents = fs2.Stream
 			.emits(List(
 				mockPartStart("foo"),
@@ -47,6 +49,10 @@ object Main extends IOApp {
 			.metered(50.millis)
 			.evalTap(e => IO.println(s"[in] $e"))
 
+		// A MultipartReceiver decides how to consume each part, then assembles the
+		// outputs from each individual part into an overall result. In this way,
+		// you can define a receiver for a specific field to expect a specific format,
+		// or use `auto` to consume all fields in a generic way.
 		val receiver: MultipartReceiver[IO, (String, Path, Map[String, MultipartReceiver.FieldValue])] = (
 			MultipartReceiver[IO].textPart("foo")(_.readString
 				.tapStart(IO.println("  [foo] start receiving text"))
@@ -54,7 +60,7 @@ object Main extends IOApp {
 				.tapRelease(IO.println("  [foo] release")),
 			),
 			MultipartReceiver[IO].filePart("file")(_.toTempFile
-				.withSizeLimit(19)
+				// .withSizeLimit(19)
 				.tapStart(IO.println("  [file] start receiving file"))
 				.tapResult(f => IO.println(s"  [file] finished writing to $f"))
 				.tapRelease(IO.println("  [file] delete temp file")),
